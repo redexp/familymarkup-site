81927d27ef6dfc88de2ddeaf4a7a188b8fc2959b
Sergii Kliuchnyk <redexp@ukr.net>
2024-12-17 06:40:46 +0200

    translate titles

--- src/lib/lang.js
+++ src/lib/lang.js
@@ -1,9 +1,25 @@
 import i18n from '../content/i18n.yaml';
 
-export default function lang(locale) {
+export default function lang(locale, text = '') {
 	const index = i18n.locales.indexOf(locale || 'en');
 
-	return function __(text) {
-		return i18n[text][index] || text;
+	const __ = function (text) {
+		return i18n[text] && i18n[text][index] || text;
 	};
+
+	if (text) {
+		return __(text);
+	}
+
+	return __;
+}
+
+export function langFrom(url) {
+	if (url instanceof URL) {
+		url = url.pathname;
+	}
+
+	const match = /^\/(\w{2})\b/.exec(url);
+
+	return match ? match[1] : 'en';
 }
